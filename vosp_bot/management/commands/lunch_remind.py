052a50b061c3693a7d7e2e0a301e5e43ae10c12b
ruslankhayrut <onsunday1703@gmail.com>
2020-01-27 15:08:41 +0300

    not subscriped exception handler

--- vosp_bot/management/commands/lunch_remind.py
+++ vosp_bot/management/commands/lunch_remind.py
@@ -9,8 +9,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             duty = Vosp.objects.get(lunch_duty_day=datetime.date.today())
-            bot.send_message(duty.telegram_id, 'Не забудьте купить вещи на ужин')
-            bot.send_message(owner_id, 'Напоминание на ужин отправлено {}'.format(duty.name))
+            if duty.telegram_id:
+                bot.send_message(duty.telegram_id, 'Не забудьте купить вещи на ужин')
+                bot.send_message(owner_id, 'Напоминание на ужин отправлено {}'.format(duty.name))
+            else:
+                bot.send_message(owner_id, 'Не получилось отправить напоминание на ужин {}, т.к. он не подписан'.format(duty.name))
         except Vosp.DoesNotExist:
             bot.send_message(owner_id, 'Не получилось отправить напоминание на ужин')
 
--- vosp_bot/management/commands/tea_remind.py
+++ vosp_bot/management/commands/tea_remind.py
@@ -9,13 +9,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             duty = Vosp.objects.get(tea_duty_day1=datetime.date.today())
-            bot.send_message(duty.telegram_id, 'Не забудьте купить вещи к чаю')
-            bot.send_message(owner_id, 'Напоминание к чаю отправлено {}'.format(duty.name))
+            if duty.telegram_id:
+                bot.send_message(duty.telegram_id, 'Не забудьте купить вещи к чаю')
+                bot.send_message(owner_id, 'Напоминание к чаю отправлено {}'.format(duty.name))
+            else:
+                bot.send_message(owner_id, 'Не получилось отправить напоминание к чаю {}, т.к. он не подписан'.format(duty.name))
+
         except Vosp.DoesNotExist:
             try:
                 duty = Vosp.objects.get(tea_duty_day2=datetime.date.today())
-                bot.send_message(duty.telegram_id, 'Не забудьте купить вещи к чаю')
-                bot.send_message(owner_id, 'Напоминание к чаю отправлено {}'.format(duty.name))
+                if duty.telegram_id:
+                    bot.send_message(duty.telegram_id, 'Не забудьте купить вещи к чаю')
+                    bot.send_message(owner_id, 'Напоминание к чаю отправлено {}'.format(duty.name))
+                else:
+                    bot.send_message(owner_id,
+                                     'Не получилось отправить напоминание к чаю {}, т.к. он не подписан'.format(
+                                         duty.name))
+
             except Vosp.DoesNotExist:
                 bot.send_message(owner_id, 'Не получилось отправить напоминание к чаю')
 
