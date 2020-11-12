83b6da8f8994338b9527d44700e68cb732b248f6
ruslankhayrut <onsunday1703@gmail.com>
2020-11-12 17:02:31 +0300

    config is not ignored

---
+++ hatim_bot/config.py
@@ -0,0 +1,4 @@
+token = '1042633149:AAFK79XzAPkcQxIB0QIYIPSWcKyIg7_ywFo'
+owner_id = '46136825'
+
+hook_url = 'https://features.li79.ru'
---
+++ vosp_bot/config.py
@@ -0,0 +1,2 @@
+token = '919717059:AAH60j_zqdIO5lIY1Lwwe8XXmKFhIb18ArE'
+owner_id = 46136825
