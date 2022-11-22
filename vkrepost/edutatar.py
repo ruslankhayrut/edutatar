2610c63a7117cb7123fd3dd25d8bb304cd136403
fayzut <fayzut@litsey79.ru>
2022-11-22 18:47:36 +0300

    daily menu - начало рефакторинга daily_menu

--- vkrepost/edutatar.py
+++ vkrepost/edutatar.py
@@ -99,98 +99,102 @@ class VKRepostManager:
         return r
 
 
-def post_page(session, page_id, data):
-    session.get("https://edu.tatar.ru")
-    url = f"https://edu.tatar.ru/admin/page/simple_page/edit/{page_id}"
-    h = {"Referer": url, "Content-Type": "application/x-www-form-urlencoded"}
 
-    file_links = ""
-    for file in data:
-        file_links = (
-            f'<p><a href="/upload/storage/org1505/files/food/{file[0]}">{file[0]}</a></p>\n'
-            + file_links
-        )
-    text = file_links
-    session.post(
-        url=url,
-        headers=h,
-        data={
-            "simple_page[title]": "Ежедневные Меню",
-            "simple_page[description]": "",
-            "simple_page[data]": text,
-            "simple_page[organization_id]": 1505,
-        },
-    )
+class MenuUploader:
+    def __init__(self, edu_session: EdutatarSession, gmail_session):
+        self.ed_session = edu_session
+        self.gmail_session = gmail_session
 
+    def post_page(self, session, page_id, data):
+        session.get("https://edu.tatar.ru")
+        url = f"https://edu.tatar.ru/admin/page/simple_page/edit/{page_id}"
+        h = {"Referer": url, "Content-Type": "application/x-www-form-urlencoded"}
 
-def get_files(session, from_folder="food"):
-    url = "https://edu.tatar.ru/js/ckfinder/core/connector/php/connector.php"
-    params = {
-        "command": "GetFiles",
-        "type": "Files",
-        "currentFolder": f"/{from_folder}/",
-    }
-    res = session.get(url, params=params)
-    files = ElementTree.fromstring(res.content).findall("Files")[0].findall("File")
-    files_list = []
-    for file in files:
-        filename = file.get("name")
-        files_list.append(
-            (
-                filename,
-                "/".join(
-                    [
-                        "https://edu.tatar.ru/upload/storage/org1505/files",
-                        from_folder,
-                        filename,
-                    ]
-                ),
+        file_links = ""
+        for file in data:
+            file_links = (
+                    f'<p><a href="/upload/storage/org1505/files/food/{file[0]}">{file[0]}</a></p>\n'
+                    + file_links
             )
+        text = file_links
+        session.post(
+            url=url,
+            headers=h,
+            data={
+                "simple_page[title]": "Ежедневные Меню",
+                "simple_page[description]": "",
+                "simple_page[data]": text,
+                "simple_page[organization_id]": 1505,
+            },
         )
-    return files_list
+
+    def get_files(self, session, from_folder="food"):
+        url = "https://edu.tatar.ru/js/ckfinder/core/connector/php/connector.php"
+        params = {
+            "command": "GetFiles",
+            "type": "Files",
+            "currentFolder": f"/{from_folder}/",
+        }
+        res = session.get(url, params=params)
+        files = ElementTree.fromstring(res.content).findall("Files")[0].findall("File")
+        files_list = []
+        for file in files:
+            filename = file.get("name")
+            files_list.append(
+                (
+                    filename,
+                    "/".join(
+                        [
+                            "https://edu.tatar.ru/upload/storage/org1505/files",
+                            from_folder,
+                            filename,
+                        ]
+                    ),
+                )
+            )
+        return files_list
+
+    def upload_file(self, session, file_path, target_folder):
+        data = {}
+        img = open(file_path, mode="rb")
+        filename = img.name.split("\\")[-1]
+        data[filename] = img
+        f = self.upload_files(session, data, target_folder)
+        img.close()
+        return f
 
 
-def upload_file(session, file_path, target_folder):
-    data = {}
-    img = open(file_path, mode="rb")
-    filename = img.name.split("\\")[-1]
-    data[filename] = img
-    f = upload_files(session, data, target_folder)
-    img.close()
-    return f
+    def upload_files(self, session, files, target_folder="food"):
+        h = {
+            "Referer": "https://edu.tatar.ru/",
+        }
+        url = "https://edu.tatar.ru/js/ckfinder/core/connector/php/connector.php"
+        params = {
+            "command": "FileUpload",
+            "type": "Files",
+            "currentFolder": f"/{target_folder}/",
+        }
+        f = []
+        for file in files:
+            f.append(session.post(url=url, headers=h, params=params, files=[file]))
+
+        return f
 
 
-def upload_files(session, files, target_folder="food"):
-    h = {
-        "Referer": "https://edu.tatar.ru/",
-    }
-    url = "https://edu.tatar.ru/js/ckfinder/core/connector/php/connector.php"
-    params = {
-        "command": "FileUpload",
-        "type": "Files",
-        "currentFolder": f"/{target_folder}/",
-    }
-    f = []
-    for file in files:
-        f.append(session.post(url=url, headers=h, params=params, files=[file]))
-
-    return f
+    def upload_multiple_files(self, session, files_list):
+        for filename in files_list:
+            print(self.upload_file(session, filename, "food"))
 
 
-def upload_multiple_files(session, files_list):
-    for filename in files_list:
-        print(upload_file(session, filename, "food"))
+    def upload_menus(self, session):
+        list_of_filenames = []
+        for i in range(1, 31):
+            day = str(i // 10) + str(i % 10)
+            filename = f"D:\\Downloads\\2021-09-{day}-sm.xlsx"
+            if os.path.exists(filename):
+                list_of_filenames.append(filename)
 
-
-def upload_menus(session):
-    list_of_filenames = []
-    for i in range(1, 31):
-        day = str(i // 10) + str(i % 10)
-        filename = f"D:\\Downloads\\2021-09-{day}-sm.xlsx"
-        if os.path.exists(filename):
-            list_of_filenames.append(filename)
-
-    upload_multiple_files(session, list_of_filenames)
+        self.upload_multiple_files(session, list_of_filenames)
 
 
 def daily_menu():
